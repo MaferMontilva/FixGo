c72e855d2108af5e16d872e3d9c30306e8e0774a
Maria Fernanda Montilva <montilvamafer88@gmail.com>
2026-07-22 14:04:11 +0200

    Permite configurar el puerto del backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
     origin: true
   });
   app.setGlobalPrefix("api");
-  await app.listen(3000);
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 
 bootstrap();
